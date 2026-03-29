09975733e5bf83cd6a5f1e288f4b9751136cd19e
Steel Maxima <steelmaxima21@gmail.com>
2026-03-29 22:00:06 +0100

    fix: edit tutorial/id page iteration 2

--- src/app/(main)/tutorials/[Id]/page.tsx
+++ src/app/(main)/tutorials/[Id]/page.tsx
@@ -2,6 +2,6 @@ export const dynamic = "force-dynamic"
 import VideoPlayer from "@/components/video/videoPlayer"
 
 export const  Page =({params}:{params:{id:string}})=>{
-
+  console.log("All params",params)
  return  <VideoPlayer id={params.id}/>
 }
